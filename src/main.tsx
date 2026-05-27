d04f7f9d2f02ee64903cac7ea2768b73ebda7484
infostaticholidays <infostaticholidays@gmail.com>
2026-05-27 10:23:52 +0100

    Add profile route

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,14 @@ import {
 
 import App from "./App";
 
+import Profile from "./pages/profile";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
