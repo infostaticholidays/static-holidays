250bf90642a034090019be86b0b4b9bfd0986876
infostaticholidays <infostaticholidays@gmail.com>
2026-05-27 20:26:38 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,8 @@ import {
 
 import App from "./App";
 import Adverts from "./pages/Adverts";
+import Shop from "./pages/Shop";
+import Properties from "./pages/Properties";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -15,6 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/adverts" element={<Adverts />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/properties" element={<Properties />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
