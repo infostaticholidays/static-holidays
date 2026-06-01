7d607c7b1f0ab9aaf04f6aab89199393fff27224
infostaticholidays <infostaticholidays@gmail.com>
2026-06-01 22:32:43 +0100

    Import HostDashboard component in App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Properties from "./pages/Properties";
 import HolidayOwners from "./pages/HolidayOwners";
 import AddProperty from "./pages/AddProperty";
 import HostLogin from "./pages/HostLogin";
+import HostDashboard from "./pages/HostDashboard";
 
 export default function App() {
   const [path, setPath] = useState(window.location.pathname);
