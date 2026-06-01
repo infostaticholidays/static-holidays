5dc888a77ccb44dfabe746bbba08fb7d5ecce7e8
infostaticholidays <infostaticholidays@gmail.com>
2026-06-01 16:28:39 +0100

    Create Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { supabase } from "../supabase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signIn() {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    alert("Logged in");
+  }
+
+  async function signUp() {
+    await supabase.auth.signUp({
+      email,
+      password,
+    });
+    alert("Account created");
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login</h1>
+
+      <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+      <br />
+
+      <input
+        placeholder="password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={signIn}>Login</button>
+      <button onClick={signUp}>Sign Up</button>
+    </div>
+  );
+}
