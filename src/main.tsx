a9f07307c34d464d6a0f359457029476ee949d3a
infostaticholidays <infostaticholidays@gmail.com>
2026-06-02 17:12:01 +0100

    Add new routes for holiday owners and host features

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,10 @@ import App from "./App";
 import Adverts from "./pages/Adverts";
 import Shop from "./pages/Shop";
 import Properties from "./pages/Properties";
+import HolidayOwners from "./pages/HolidayOwners";
+import HostLogin from "./pages/HostLogin";
+import HostDashboard from "./pages/HostDashboard";
+import AddProperty from "./pages/AddProperty";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -18,7 +22,18 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/adverts" element={<Adverts />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/properties" element={<Properties />} />
+        <Route path="/properties" element={<Properties />} 
+          <Routes>
+  <Route path="/" element={<App />} />
+  <Route path="/adverts" element={<Adverts />} />
+  <Route path="/shop" element={<Shop />} />
+  <Route path="/properties" element={<Properties />} />
+
+  <Route path="/holidayowners" element={<HolidayOwners />} />
+  <Route path="/host-login" element={<HostLogin />} />
+  <Route path="/host-dashboard" element={<HostDashboard />} />
+  <Route path="/add-property" element={<AddProperty />} />
+</Routes>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
