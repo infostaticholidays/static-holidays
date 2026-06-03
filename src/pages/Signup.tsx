16db25a808683476e2cba0b01f8b8603c3588ad5
infostaticholidays <infostaticholidays@gmail.com>
2026-06-03 22:40:49 +0100

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,1 +1,11 @@
+const { data, error } = await supabase.auth.signUp({
+  email: email.trim(),
+  password: password.trim(),
+});
 
+console.log("SIGNUP ERROR:", error);
+
+if (error) {
+  alert(error.message);
+  return;
+}
