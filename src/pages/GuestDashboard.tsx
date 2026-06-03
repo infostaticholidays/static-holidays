0110a73d03575bd9b040652c2b0f83a527d5452a
infostaticholidays <infostaticholidays@gmail.com>
2026-06-03 17:09:37 +0100

    Create GuestDashboard.tsx

---
+++ src/pages/GuestDashboard.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function Dashboard() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+    });
+  }, []);
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dashboard</h1>
+
+      {user ? (
+        <>
+          <p>Logged in as:</p>
+          <p>{user.email}</p>
+        </>
+      ) : (
+        <p>Not logged in</p>
+      )}
+    </div>
+  );
+}
