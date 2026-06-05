5c479c125b211d3d469b1e3e3fe1cf69695008e8
infostaticholidays <infostaticholidays@gmail.com>
2026-06-05 07:53:11 +0100

    Update HostLogin.tsx

--- src/pages/HostLogin.tsx
+++ src/pages/HostLogin.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../supabase";
+import { supabase } from "../lib/supabase";
 
 export default function HostLogin() {
   const [email, setEmail] = useState("");
