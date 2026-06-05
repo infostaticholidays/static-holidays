47c68b119278144be059bd1d8abb12e4b218aa14
infostaticholidays <infostaticholidays@gmail.com>
2026-06-05 07:35:08 +0100

    Remove duplicate Login and Sign Up links

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,6 @@ export default function App() {
         <Link style={btn} to="/host-login">Become a Host</Link>
         <Link style={btn} to="/login">Login</Link>
         <Link style={btn} to="/signup">Sign Up</Link>
-        <Link style={btn} to="/login">Login</Link>
-        <Link style={btn} to="/signup">Sign Up</Link>
         <Link style={btn} to="/account">My Account</Link>
       </header>
 
