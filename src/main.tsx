9ee4a9ee7f049bcc637dc2e13c911da4cbc124d1
infostaticholidays <infostaticholidays@gmail.com>
2026-06-10 16:05:52 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,101 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import Adverts from "./pages/Adverts";
-import Shop from "./pages/Shop";
-import Properties from "./pages/Properties";
-import HolidayOwners from "./pages/HolidayOwners";
-import HostLogin from "./pages/HostLogin";
-import HostDashboard from "./pages/HostDashboard";
-import GuestDashboard from "./pages/GuestDashboard";
-import AddProperty from "./pages/AddProperty";
-import PropertyDetail from "./pages/PropertyDetail";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import PropertyCalendar from "./pages/PropertyCalendar";
-import Account from "./pages/Account";
 
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-
-        <Route
-          path="/adverts"
-          element={<Adverts />}
-        />
-
-        <Route
-          path="/shop"
-          element={<Shop />}
-        />
-
-        <Route
-          path="/properties"
-          element={<Properties />}
-        />
-
-        <Route
-          path="/property/:id"
-          element={<PropertyDetail />}
-        />
-
-        <Route
-          path="/holidayowners"
-          element={<HolidayOwners />}
-        />
-
-   
-
-        <Route
-          path="/host-login"
-          element={<HostLogin />}
-        />
-
-        <Route
-          path="/host-dashboard"
-          element={<HostDashboard />}
-        />
-
-        <Route
-          path="/guest-dashboard"
-          element={<GuestDashboard />}
-        />
-
-        <Route
-          path="/calendar/:propertyId"
-          element={<PropertyCalendar />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/signup"
-          element={<Signup />}
-        />
-
-        <Route
-  path="/account"
-  element={<Account />}
-/>
-
-        <Route
-          path="/add-property"
-          element={<AddProperty />}
-        />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
