07ec6914156c7afa1783b9ffb27c46cadaab90ed
infostaticholidays <infostaticholidays@gmail.com>
2026-06-15 15:05:51 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import { Link, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Account from "./pages/Account";
-import GuestDashboard from "./pages/GuestDashboard";
-import HostDashboard from "./pages/HostDashboard";
 import Properties from "./pages/Properties";
+import PropertyDetail from "./pages/PropertyDetail";
 import Adverts from "./pages/Adverts";
 import Shop from "./pages/Shop";
 import HolidayOwners from "./pages/HolidayOwners";
 import HostLogin from "./pages/HostLogin";
-import PropertyDetail from "./pages/PropertyDetail";
-import GuestAccount from "./pages/GuestAccount";
-import OwnerAccount from "./pages/OwnerAccount";
+
+import GuestDashboard from "./pages/GuestDashboard";
+import HostDashboard from "./pages/HostDashboard";
+
+import Events from "./pages/Events";
+import EventDetail from "./pages/EventDetail";
+
+import Bookings from "./pages/Bookings";
+import BookingDetail from "./pages/BookingDetail";
+
+import Profile from "./pages/Profile";
+import AddProperty from "./pages/AddProperty";
+import PropertyCalendar from "./pages/PropertyCalendar";
+import ShopDetail from "./pages/ShopDetail";
 
 export default function App() {
   return (
@@ -25,7 +34,6 @@ export default function App() {
         flexDirection: "column",
       }}
     >
-      {/* HEADER */}
       <header
         style={{
           background: "#14532d",
@@ -37,46 +45,63 @@ export default function App() {
         }}
       >
         <Link style={btn} to="/">Home</Link>
+
         <Link style={btn} to="/properties">Properties</Link>
+
         <Link style={btn} to="/adverts">Adverts</Link>
+
         <Link style={btn} to="/shop">Shop</Link>
+
         <Link style={btn} to="/holidayowners">Holiday Owners</Link>
-        <Link style={btn} to="/host-login">Become a Host</Link>
+
         <Link style={btn} to="/login">Login</Link>
+
         <Link style={btn} to="/signup">Sign Up</Link>
-        <Link style={btn} to="/account">My Account</Link>
       </header>
 
-      {/* PAGE CONTENT */}
-      <main
-        style={{
-          flex: 1,
-          padding: "40px",
-        }}
-      >
+      <main style={{ flex: 1, padding: "40px" }}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
 
-          <Route path="/properties" element={<Properties />} />
-          <Route path="/adverts" element={<Adverts />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/holidayowners" element={<HolidayOwners />} />
-          <Route path="/host-login" element={<HostLogin />} />
+          <Route path="/" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/signup" element={<Signup />} />
 
           <Route path="/guest-dashboard" element={<GuestDashboard />} />
-          <Route path="/host-dashboard" element={<HostDashboard />} />
-          <Route path="/guest-account"  element={<GuestAccount />}
-            />
 
-<Route
-  path="/owner-account"
-  element={<OwnerAccount />}
-  />
+          <Route path="/host-dashboard" element={<HostDashboard />} />
+
+          <Route path="/properties" element={<Properties />} />
 
           <Route path="/property/:id" element={<PropertyDetail />} />
+
+          <Route path="/adverts" element={<Adverts />} />
+
+          <Route path="/shop" element={<Shop />} />
+
+          <Route path="/shop/:id" element={<ShopDetail />} />
+
+          <Route path="/holidayowners" element={<HolidayOwners />} />
+
+          <Route path="/host-login" element={<HostLogin />} />
+
+          <Route path="/events" element={<Events />} />
+
+          <Route path="/events/:id" element={<EventDetail />} />
+
+          <Route path="/bookings" element={<Bookings />} />
+
+          <Route path="/booking/:id" element={<BookingDetail />} />
+
+          <Route path="/profile" element={<Profile />} />
+
+          <Route path="/add-property" element={<AddProperty />} />
+
+          <Route
+            path="/property-calendar"
+            element={<PropertyCalendar />}
+          />
 
           <Route
             path="*"
@@ -87,10 +112,10 @@ export default function App() {
               </div>
             }
           />
+
         </Routes>
       </main>
 
-      {/* FOOTER */}
       <footer
         style={{
           background: "#14532d",
