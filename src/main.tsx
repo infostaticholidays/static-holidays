cdde2d460ce0aba4676fa4bb83851cefe4069c70
infostaticholidays <infostaticholidays@gmail.com>
2026-06-16 14:23:18 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import Adverts from "./pages/Adverts";
-import Shop from "./pages/Shop";
-import Properties from "./pages/Properties";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/adverts" element={<Adverts />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/properties" element={<Properties />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
