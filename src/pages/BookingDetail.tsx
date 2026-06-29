e26c6711a4ca9b115208b64114dce226aa4bdb35
infostaticholidays <infostaticholidays@gmail.com>
2026-06-29 14:11:09 +0100

    Update BookingDetail.tsx

--- src/pages/BookingDetail.tsx
+++ src/pages/BookingDetail.tsx
@@ -1,3 +1,5 @@
+import GuestReviewForm from "../components/GuestReviewForm";
+
 export default function BookingDetail() {
   const path = window.location.pathname;
   const id = path.split("/").pop();
@@ -5,14 +7,17 @@ export default function BookingDetail() {
   return (
     <div style={{ padding: "40px" }}>
       <h1>Booking #{id}</h1>
-      <GuestReviewForm
-  bookingId={booking.id}
-  propertyId={booking.property_id}
-  reviewerId={user.id}
-  reviewedUserId={booking.owner_id}
-/>
 
       <p>This is a demo booking detail page.</p>
+
+      <GuestReviewForm
+        bookingId=""
+        propertyId=""
+        reviewerId=""
+        reviewedUserId=""
+      />
+
+      <br />
 
       <a href="/bookings">← Back to Bookings</a>
     </div>
