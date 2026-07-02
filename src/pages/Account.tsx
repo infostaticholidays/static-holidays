6deff530733d1cd08f79f8be1e4fc964e4214202
infostaticholidays <infostaticholidays@gmail.com>
2026-07-02 11:35:31 +0100

    Update Account.tsx

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,4 +1,4 @@
-mport { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
 
